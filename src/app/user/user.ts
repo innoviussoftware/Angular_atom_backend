ebdd8343a62832b4131647503d9ce50dc96f1626
Vishal Gosia <vishal.innovius@gmail.com>
2019-01-04 16:39:38 +0530

    ..

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -3,9 +3,19 @@ export class User {
   id: number;
   name: string;
   email: string;
+  last_name: string;
   email_verified_at:string;
-    roles:any[];
-    courses:Course[];
-    updated_at: string;
-    created_at: string;
+  contact_number:string;
+  short_description:string;
+  biography:string;
+  facebook:string;
+  twitter:string;
+  google:string;
+  pinterest:string;
+  updated_at: string;
+  created_at: string;
+  token: string;
+  courses: Course[];
+  skype:string;
+
 }
